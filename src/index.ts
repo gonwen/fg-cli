3c32db30531c0a6e14a8e89bb96e350c5c262d4e
gongwen <gongwen_1@163.com>
2019-09-21 17:30:34 +0800

    doc:

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,6 @@ program.command('init (template)')
     })
 program.parse(process.argv)
 if(program.args.length==0){
-    //这里是处理默认没有输入参数或者命令的时候，显示help信息
+    // help info -h -*
     program.help();
 }
